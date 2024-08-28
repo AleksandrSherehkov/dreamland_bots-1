5918a44fbe8b78b77bb5de1dd09870dc42fe73d5
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-08-28 03:12:50 +0300

    feat: add /help [id] command and integrate with standard /help

--- telegram/commandArgs.js
+++ telegram/commandArgs.js
@@ -1,31 +1,30 @@
 const commandArgs = () => (ctx, next) => {
-    if (ctx.updateType === 'message' && ctx.updateSubTypes.includes('text')) {
-        const text = ctx.update.message.text.toLowerCase();
+  if (ctx.updateType === 'message' && ctx.updateSubTypes.includes('text')) {
+    const text = ctx.update.message.text.toLowerCase();
 
-        if (text.startsWith('/')) {
-            const match = text.match(/^\/([^\s]+)\s*(.+)?/);
-            let args = [];
-            let command;
+    if (text.startsWith('/')) {
+      const match = text.match(/^\/([^\s]+)\s*(.+)?/);
+      let args = [];
+      let command;
 
-            if (match !== null) {
-                if (match[1]) {
-                    command = match[1];
-                }
-                if (match[2]) {
-                    args = match[2];
-                }
-            }
-
-            ctx.state.command = {
-                raw: text,
-                command,
-                args
-            };
+      if (match !== null) {
+        if (match[1]) {
+          command = match[1];
         }
-    }
+        if (match[2]) {
+          args = match[2];
+        }
+      }
 
-    return next();
+      ctx.state.command = {
+        raw: text,
+        command,
+        args,
+      };
+    }
+  }
+
+  return next();
 };
 
-module.exports = commandArgs
-
+module.exports = commandArgs;
